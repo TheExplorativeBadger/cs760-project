8977187371d3eba042810b0934bb79ecb6716e86
Jacob Lorenz <58476572+TheExplorativeBadger@users.noreply.github.com>
2020-11-11 12:09:14 -0600

    initial commit, uploading files for the Data Transformation program

---
+++ RawDataToUsableDataConverter/README.md
@@ -0,0 +1,30 @@
+# RawDataToUsableDataConverter
+A java program that works to transform raw data files into an aggregate feature vector / response matrix that can be used in further ML algorithms.
+
+The program requires 3 files to work:
+1. StartingFiles/CovidFileWithDates.csv 
+    * The raw county COVID data obtained from 
+    https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+    that is used to derive the severity index of the COVID response for each county
+
+2. StartingFiles/RawCountyDemographics.csv
+    * The raw demographics data obtained from 
+    [public.opendatasoft.com](https://public.opendatasoft.com/explore/dataset/usa-2016-presidential-election-by-county/table/?disjunctive.state&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQVZHIiwieUF4aXMiOiJjYSIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6IiNFOTFEMEUifSx7InR5cGUiOiJjb2x1bW4iLCJmdW5jIjoiQVZHIiwieUF4aXMiOiJkZW0xNl9mcmFjIiwic2NpZW50aWZpY0Rpc3BsYXkiOnRydWUsImNvbG9yIjoiIzIzMjA2NiJ9XSwieEF4aXMiOiJzdGF0ZSIsIm1heHBvaW50cyI6MjAwLCJzb3J0IjoiIiwiY29uZmlnIjp7ImRhdGFzZXQiOiJ1c2EtMjAxNi1wcmVzaWRlbnRpYWwtZWxlY3Rpb24tYnktY291bnR5Iiwib3B0aW9ucyI6eyJkaXNqdW5jdGl2ZS5zdGF0ZSI6dHJ1ZX19fV0sInRpbWVzY2FsZSI6IiIsImRpc3BsYXlMZWdlbmQiOnRydWUsImFsaWduTW9udGgiOnRydWV9)
+    that is used to build the final feature vectors for each county.
+    
+3. StartingFiles/FeaturesToKeep.csv
+    * A list (1 index per line) of all feature indices from the RawCountyDemographics.csv file
+    that you want to keep for your final resulting feature matrix
+    
+    
+After processing these files, the program will produce a number of intermediary files, as well as one named
+* FinalCombinedFeaturesWithSeverity.csv
+
+This file will be what gets carried on to the other steps of our project to perform various ML algorithms on it. Think of this file
+as the equivalent of what we begin with when looking at the Titanic Data sets.
+
+NOTE: All Intermediary files outside of /StartingFiles, as well as the entire /TimeSeries directory should be deleted after each run.
+Additionally, a fresh src/TimeSeries directory needs to be created before each time you run this program.
+
+
+
---
+++ RawDataToUsableDataConverter/src/RawDataToUsableDataConverter.java
@@ -0,0 +1,352 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class RawDataToUsableDataConverter {
+    public static void main(String[] args){
+
+        // Transform the feature vectors into strictly numerical vectors
+        transformFeatureVectorData();
+
+        // Transform the responses into severity indices
+        transformResponseDate();
+        // Todo: Figure out an equation / way to turn the data into a severity index
+
+        // Match the transformed feature vectors with their corresponding severity index, create 1 csv
+        combineFeaturesWithSeverityIndex();
+
+        // Use this file in Matlab to perform Linear Regression.
+    }
+
+    /**
+     *******************************************************
+     * The following methods are used to transform the
+     * feature vector data
+     *******************************************************
+     */
+
+    // Question: What to do about empty data?
+    // Question: What other processing / transformation on feature data?
+    // - Go through and identify the features you actually care about - DONE SEE FeaturesToKeep.csv
+    // NOTE: We will need certain feature information from each county to perform the significance calculation
+    public static void transformFeatureVectorData() {
+        removeUnwantedFeatureValues();
+
+        /**
+         * TODO: Perform any other feature vector transformation here
+         */
+
+    }
+
+    public static void removeUnwantedFeatureValues() {
+        try {
+
+            BufferedReader csvReader = new BufferedReader(new FileReader("src/StartingFiles/FeaturesToKeep.csv"));
+            Set<Integer> wantedFeatures = new HashSet<>();
+            String curLine = "";
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(",");
+                Integer curFeatureIndex = Integer.parseInt(data[0]);
+                wantedFeatures.add(curFeatureIndex);
+            }
+            csvReader.close();
+
+            csvReader = new BufferedReader(new FileReader("src/StartingFiles/RawCountyDemographics.csv"));
+            FileWriter csvWriter = new FileWriter("src/FinalFeatureVectors.csv");
+            curLine = "";
+            String firstLine = csvReader.readLine();
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(";");
+                boolean addVector = true;
+                String stringBuilder = data[2];
+                // Remove indices 0, 1, 3, 107, 108, 147
+                for (int i = 0; i < data.length; i ++) {
+                    if (wantedFeatures.contains(i) && i != 2) {
+                        // We want to add the remaining fields to
+                        stringBuilder += ";" + data[i].trim();
+                        if (data[i].trim().equals("")) {
+                            //addVector = false;
+                            // This condition would eliminate all but 800ish records :/
+                        }
+                    } else {
+                        // Do nothing, we do not want these fields in the matrix
+                    }
+                }
+                stringBuilder += "\n";
+                if (addVector) {
+                    csvWriter.append(stringBuilder);
+                }
+            }
+            csvReader.close();
+            csvWriter.flush();
+            csvWriter.close();
+
+        } catch (IOException d) {
+            d.printStackTrace();
+        }
+    }
+
+    /**
+     * End feature vector data transformation methods
+     *******************************************************
+     */
+
+    /**
+     *******************************************************
+     * The following methods are used to transform the
+     * response data
+     *******************************************************
+     */
+
+    public static void transformResponseDate() {
+        modifyDataFileFormat();
+        Set<Integer> countyNumbers = separateDataByCounties();
+        calculateSeverityIndices(countyNumbers);
+    }
+
+    public static void modifyDataFileFormat() {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader("src/StartingFiles/CovidFileWithDates.csv"));
+            FileWriter csvWriter = new FileWriter("src/CovidFileWithModifiedDates.csv");
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date day0 = format.parse ( "2020-01-21" );
+            long msInDay = 86400000;
+            long day0Time = day0.getTime();
+            String curLine = "";
+            String firstLine = csvReader.readLine();
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(",");
+
+                if (data[0].trim().equals("") ||
+                        data[3].trim().equals("") ||
+                        data[4].trim().equals("") ||
+                        data[5].trim().equals("")) {
+                    // System.out.println("Inside empty row");
+                    continue;
+                }
+
+                Date curDate = format.parse (data[0]);
+                long curDateTime = curDate.getTime();
+
+                long timeBetween = curDateTime - day0Time;
+                int numberDays = (int)(timeBetween / msInDay);
+                String numberDaysString = String.valueOf(numberDays);
+
+                csvWriter.append(numberDaysString);
+                csvWriter.append(",");
+                csvWriter.append(data[3]);
+                csvWriter.append(",");
+                csvWriter.append(data[4]);
+                csvWriter.append(",");
+                csvWriter.append(data[5]);
+                csvWriter.append("\n");
+            }
+            csvReader.close();
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (IOException | ParseException d) {
+            d.printStackTrace();
+        }
+    }
+
+    public static Set<Integer> separateDataByCounties() {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader("src/CovidFileWithModifiedDates.csv"));
+
+            // Q: Which data structure to use as mapping?
+            // We are mapping Integer (county#) to a collection of its corresponding data points
+            Map<Integer, List<String>> byCountyFiles = new HashMap<>();
+            Set<Integer> countiesBeingTracked = new HashSet<>();
+
+            String curLine = "";
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(",");
+
+                Integer countyNumber = Integer.parseInt(data[1]);
+
+                // Using the county number, add the curLine into the ArrayList at that key
+                if (byCountyFiles.containsKey(countyNumber)) {
+                    byCountyFiles.get(countyNumber).add(curLine);
+                } else {
+                    List<String> starterList = new ArrayList<>();
+                    starterList.add(curLine);
+                    countiesBeingTracked.add(countyNumber);
+                    byCountyFiles.put(countyNumber, starterList);
+                }
+            }
+            csvReader.close();
+
+            // System.out.println(countiesBeingTracked.size());
+            // We now theoretically have a map containing all the individual time series
+            // What to do?
+            // Convert the cumulative to daily increase
+            for (Integer county : countiesBeingTracked) {
+                FileWriter csvWriter = new FileWriter("src/TimeSeries/" + county.toString() + ".csv");
+                List<String> curCountyTimeSeries = byCountyFiles.get(county);
+                // System.out.println(curCountyTimeSeries.size());
+                int prevCases = 0;
+                int prevDeaths = 0;
+                // #,#,#,#
+                for (String curDataLine: curCountyTimeSeries) {
+                    String[] data = curDataLine.split(",");
+                    // 0 - DayNumber
+                    int curDay = Integer.parseInt(data[0]);
+                    // 1 - CountyNumber
+                    int countyNum = Integer.parseInt(data[1]);
+                    // 2 - NumberTotalCases
+                    int curCases = Integer.parseInt(data[2]);
+                    // 3 - NumberTotalDeaths
+                    int curDeaths = Integer.parseInt(data[3]);
+
+                    int deltaCases = curCases - prevCases;
+                    int deltaDeaths = curDeaths - prevDeaths;
+
+                    // We now have all the data + the rolling cases
+
+                    csvWriter.append(data[0]);
+                    csvWriter.append(",");
+                    csvWriter.append(data[1]);
+                    csvWriter.append(",");
+                    csvWriter.append(String.valueOf(deltaCases));
+                    csvWriter.append(",");
+                    csvWriter.append(String.valueOf(data[2]));
+                    csvWriter.append(",");
+                    csvWriter.append(String.valueOf(deltaDeaths));
+                    csvWriter.append(",");
+                    csvWriter.append(String.valueOf(data[3]));
+                    csvWriter.append("\n");
+
+                    prevCases = curCases;
+                    prevDeaths = curDeaths;
+
+                }
+
+                csvWriter.flush();
+                csvWriter.close();
+            }
+
+            return countiesBeingTracked;
+
+        } catch (IOException d) {
+            d.printStackTrace();
+        }
+        return null;
+    }
+
+    // Todo: Figure out an equation / way to turn the data into a severity index
+    public static void calculateSeverityIndices(Set<Integer> countyNumbers) {
+        try {
+
+            // Keep track of the severity indices in a data structure here, going to print after everything done
+            List<String> resultList = new ArrayList<>();
+
+            for (Integer curCountyNumber: countyNumbers) {
+                BufferedReader csvReader = new BufferedReader(new FileReader("src/TimeSeries/" + curCountyNumber + ".csv"));
+
+                double severityIndex = 0.00;
+                String curLine = "";
+                while ((curLine = csvReader.readLine()) != null) {
+                    String[] data = curLine.split(",");
+
+                    int curDay = Integer.parseInt(data[0]);
+                    int countyNum = Integer.parseInt(data[1]);
+                    int deltaCases = Integer.parseInt(data[2]);
+                    int totalCases = Integer.parseInt(data[3]);
+                    int deltaDeaths = Integer.parseInt(data[4]);
+                    int totalDeaths = Integer.parseInt(data[5]);
+
+                    /**
+                     * TODO: Need to implement a function to calculate severity here
+                     */
+
+                    severityIndex = 1;
+
+                }
+                csvReader.close();
+
+                String curResponseEntry = curCountyNumber + "," + severityIndex + "\n";
+                resultList.add(curResponseEntry);
+            }
+
+            FileWriter csvWriter = new FileWriter("src/CovidSeverityIndices.csv");
+            for (String response: resultList) {
+                csvWriter.append(response);
+            }
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (IOException d) {
+
+        }
+    }
+
+    /**
+     * End response data transformation methods
+     *******************************************************
+     */
+
+    /**
+     *******************************************************
+     * The following methods are used to combine the final
+     * feature vector set with the appropriate severity
+     * level. The resulting file will be usable in further
+     * Linear Regression algorithms
+     *******************************************************
+     */
+
+    public static void combineFeaturesWithSeverityIndex() {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader("src/FinalFeatureVectors.csv"));
+            Map<Integer, String> countyDemographicsMap = new HashMap<>();
+            String curLine = "";
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(";");
+                int countyNumber = Integer.parseInt(data[0]);
+                countyDemographicsMap.put(countyNumber, curLine);
+            }
+            csvReader.close();
+
+            csvReader = new BufferedReader(new FileReader("src/CovidSeverityIndices.csv"));
+            Map<Integer, Double> countyCovidSeverityMap = new HashMap<>();
+            List<Integer> countyNumberList = new ArrayList<>();
+            curLine = "";
+            while ((curLine = csvReader.readLine()) != null) {
+                String[] data = curLine.split(",");
+                int countyNumber = Integer.parseInt(data[0]);
+                countyNumberList.add(countyNumber);
+                countyCovidSeverityMap.put(countyNumber, Double.parseDouble(data[1]));
+            }
+            csvReader.close();
+
+            FileWriter csvWriter = new FileWriter("src/FinalCombinedFeaturesWithSeverity.csv");
+
+            for (Integer countyNumber: countyNumberList) {
+                double curCountySeverityIndex = countyCovidSeverityMap.get(countyNumber);
+                String curCountyFeatureVector = countyDemographicsMap.get(countyNumber);
+
+                if (curCountyFeatureVector != null) {
+                    csvWriter.append(String.valueOf(curCountySeverityIndex));
+                    csvWriter.append(";");
+                    csvWriter.append(curCountyFeatureVector);
+                    csvWriter.append("\n");
+                }
+
+            }
+            csvWriter.flush();
+            csvWriter.close();
+
+        } catch (IOException d) {
+            d.printStackTrace();
+        }
+    }
+
+    /**
+     * End feature / response combination methods
+     *******************************************************
+     */
+
+}
